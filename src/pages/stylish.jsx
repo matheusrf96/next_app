5c5f46919a742dfe7f8c9cc9c2b5bb044429547f
Matheus Rodrigues <matheusrf96@gmail.com>
2021-09-27 19:00:32 -0300

    Create styles component

---
+++ src/pages/stylish.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/stylish.module.css'
+
+
+export default function Stylish() {
+    return (
+        <div className={ styles.purple }>
+            <h1>Estilo usando CSS módulos</h1>
+        </div>
+    )
+}
