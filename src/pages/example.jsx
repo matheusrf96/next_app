9dbba3464b37086a99d033eeb8333400fa7d5744
Matheus Rodrigues <matheusrf96@gmail.com>
2021-09-28 19:10:05 -0300

    Use props

--- src/pages/example.jsx
+++ src/pages/example.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 export default function Example() {
     return (
         <div>
-            <Header />
+            <Header title="Fundamentos de Next.js & React" />
             <h1>Exemplo</h1>
         </div>
     )
