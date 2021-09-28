ed82a25a0de822f796347162eec6e5c9a97b6722
Matheus Rodrigues <matheusrf96@gmail.com>
2021-09-28 18:54:43 -0300

    Create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Fundamentos de Next.js & React</h1>
+        </header>
+    )
+}
---
+++ src/pages/example.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function Example() {
+    return (
+        <div>
+            <Header />
+            <h1>Exemplo</h1>
+        </div>
+    )
+}
